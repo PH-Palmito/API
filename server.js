17d72234c43b1933e2fdce7545f2aac101c768eb
PH-Palmito <phpalmito@gmail.com>
2024-12-28 16:58:00 -0300

    primeira parte do projeto node

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+    users.push(req.body)
+
+    res.send('ok aqui deu certo')
+
+})
+
+
+app.get('/usuarios', (req, res) => {
+res.json(users)
+})
+
+app.listen(3000)
+
+
